6de7c21e2547d3fd71560ed923f70478319ac82a
taross <taross14.selkie@gmail.com>
2018-08-05 20:12:00 +0900

    Initial Commit

---
+++ Multa/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Multa
+//
+//  Created by Taro Sato on 2018/08/05.
+//  Copyright © 2018年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MultaTests/MultaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MultaTests.swift
+//  MultaTests
+//
+//  Created by Taro Sato on 2018/08/05.
+//  Copyright © 2018年 Taro Sato. All rights reserved.
+//
+
+import XCTest
+@testable import Multa
+
+class MultaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
